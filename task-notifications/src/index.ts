58760f511649f18f37b1e2cf2388adc1ab0c4b37
BitsHeart <ga_bennecer@esi.dz>
2021-06-07 17:43:13 +0100

    feat: add task-notification for mobile app with FCM

--- task-notifications/src/index.ts
+++ task-notifications/src/index.ts
@@ -18,16 +18,16 @@ import { Connection, createConnection } from "typeorm";
 import * as swaggerUi from "swagger-ui-express";
 import * as swaggerDocument from "../swagger.json";
 import * as express from "express";
-import { json } from "express";
+import { json, urlencoded } from "express";
 import * as cors from "cors";
 import * as morgan from "morgan";
 import Router from "./routes";
 
+
 const app: express.Application = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
 
 app.use(json());
+app.use(urlencoded({extended: true}))
 app.use(cors());
 app.use(morgan("dev"));
 
@@ -36,18 +36,16 @@ app.use("/notifDoc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(Router);
 
-const PORT = 8000;
 
-// start server and socket event listener
-http.listen(PORT, () => {
-  console.log(
-    `🚀 Task Notifications --> 🏠 LocalHost:${
-      process.env.PORT || 8080
-    } || 🐳 Docker:8002 `
-  );
-  /* io.on("connection", (socket: any) => {
-    socket.on("messageSent", (message: any) => {
-      socket.broadcast.emit("messageSent", message);
+
+const PORT = process.env.PORT || 8083;
+
+createConnection()
+  .then(async (_connection: Connection) => {
+    app.listen(PORT, () => {
+      console.log(
+        `🚀 Task Notifications --> 🏠 LocalHost:${PORT} || 🐳 Docker:8002 `
+      );
     });
-  }); */
-});
+  })
+  .catch((error) => console.log(error));
--- task-notifications/src/routes/index.ts
+++ task-notifications/src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { addATNotif, deleteATNotif, getATNotif, getATNotifs, updateATNotif } from "../controllers/agentTokenNotif";
 import {
   addTaskNotif,
   getTaskNotif,
@@ -6,6 +7,7 @@ import {
   deleteTaskNotif,
   get,
 } from "../controllers/taskNotification";
+let FCM = require("fcm-node")
 
 const router = Router();
 
@@ -20,5 +22,51 @@ router.post("/taskNotif", addTaskNotif);
 router.put("/taskNotif/:id", markAsRead);
 
 router.delete("/taskNotif/:id", deleteTaskNotif);
+
+// AgentToken Notification
+
+
+router.get("/ATNotif", getATNotifs);
+
+router.post("/ATNotif", addATNotif);
+
+router.put("/ATNotif/", updateATNotif);
+
+router.delete("/ATNotif/:id", deleteATNotif);
+
+router.get("/ATNotif/:id", getATNotif);
+
+
+// FCM Route 
+
+const SERVER_KEY = "AAAASweisnY:APA91bEoxK-8mSVbQWBai4_fO7Hn9wcS2R1UCcoMJXboxLKiGAdmj5H2Le7hto97F5s56l6zxSULPwrHinlHQdyw47Cda1WMiAKGG09pD_oNi5E3TakZW1BuySY_Jjy-8J3n8N7_KHQ6"
+
+router.post("/fcm", async (req, res, next) => {
+  try {
+    let fcm = new FCM(SERVER_KEY);
+    
+    let message = {
+      to: "eqmcyT4lR3i4CgBDYeZ38W:APA91bH3AwGQ_OUdvs5w8UFhiWJiw2-Y5tHhDgqFRRmwRtm4LGf9A8fiiXCCpFyXCUTbB8OrEayB4QFHm_G8CoC4N48FbAI9YfuuewyWhA1xLTmgpKfRAKQYRHE1NekXMm_jtC1lxDGs",
+      notification: {
+        title: req.body.title,
+        body: req.body.body,
+        sound: "default",
+        "click_action": "FCM_PLUGIN_ACTIVITY"
+
+      }
+    }
+
+    fcm.send(message,(err:any,response:any) =>{
+      if(err){
+        next(err);
+      } else {
+        res.json(response)
+      }
+    })
+
+  } catch (error) {
+    next(error)
+  }
+})
 
 export default router;
